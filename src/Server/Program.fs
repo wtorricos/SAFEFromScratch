c8bebbba8494cbb09efb9e267589f36012203ba0
Walter Torricos <ticot55@gmail.com>
2021-11-23 23:56:57 -0400

    4. Add integration tests with WebApplicationfactory.

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,18 +1,24 @@
-﻿module Program
+﻿module Main
 
+open System
 open Giraffe
 open Saturn
 
 let greet name = sprintf $"Hello {name} from Saturn!"
 
-let routes = router {
-    get "/api/foo" (text "Hello from Saturn!")
-    getf "/api/foo/%s" (fun n -> n |> greet |> text)
-}
-
-let app =
-    application {
-        use_router routes
+// The type program is used as the entry point for WebApplicationFactory for testing.
+type Program () =
+    let routes = router {
+        get "/api/foo" (text "Hello from Saturn!")
+        getf "/api/foo/%s" (fun n -> n |> greet |> text)
     }
 
-run app
+    let app =
+        application {
+            use_router routes
+        }
+
+    member x.main (_: string array) = run app
+
+
+Program().main [||]
